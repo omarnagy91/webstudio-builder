webstudio-is/webstudio

adding type switcher to assets manager, realized the design needs discussion

apps/designer/app/designer/features/workspace/canvas-tools/text-toolbar/text-toolbar.tsx
@@ -4,7 +4,11 @@ import {
   useSelectedInstanceData,
   useSelectionRect,
 } from "~/designer/shared/nano-states";
-import { ToggleGroup, type CSS } from "@webstudio-is/design-system";
+import {
+  ToggleGroupRoot,
+  ToggleGroupItem,
+  type CSS,
+} from "@webstudio-is/design-system";
 import { FontBoldIcon, FontItalicIcon, Link2Icon } from "@webstudio-is/icons";
 import { createInstance } from "~/shared/tree-utils";
 
@@ -55,7 +59,7 @@ type ToolbarProps = {
 
 const Toolbar = ({ css, onValueChange, rootRef }: ToolbarProps) => {
   return (
-    <ToggleGroup.Root
+    <ToggleGroupRoot
       ref={rootRef}
       type="single"
       onValueChange={onValueChange}
@@ -68,16 +72,16 @@ const Toolbar = ({ css, onValueChange, rootRef }: ToolbarProps) => {
         ...css,
       }}
     >
-      <ToggleGroup.Item value="Bold">
+      <ToggleGroupItem value="Bold">
         <FontBoldIcon />
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="Italic">
+      </ToggleGroupItem>
+      <ToggleGroupItem value="Italic">
         <FontItalicIcon />
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="Link">
+      </ToggleGroupItem>
+      <ToggleGroupItem value="Link">
         <Link2Icon />
-      </ToggleGroup.Item>
-    </ToggleGroup.Root>
+      </ToggleGroupItem>
+    </ToggleGroupRoot>
   );
 };
 

omarnagy91/webstudio-builder

apps/designer/app/designer/features/workspace/canvas-tools/text-toolbar/text-toolbar.tsx
@@ -5,7 +5,11 @@ import {
   type TextToolbarState,
   useTextToolbarState,
 } from "~/designer/shared/nano-states";
-import { ToggleGroup, type CSS } from "@webstudio-is/design-system";
+import {
+  ToggleGroupRoot,
+  ToggleGroupItem,
+  type CSS,
+} from "@webstudio-is/design-system";
 import { FontBoldIcon, FontItalicIcon, Link2Icon } from "@webstudio-is/icons";
 import { useSubscribe } from "~/shared/pubsub";
 
@@ -81,7 +85,7 @@ const Toolbar = ({ css, rootRef, state, onToggle }: ToolbarProps) => {
     value.push("link");
   }
   return (
-    <ToggleGroup.Root
+    <ToggleGroupRoot
       ref={rootRef}
       type="multiple"
       value={value}
@@ -106,16 +110,16 @@ const Toolbar = ({ css, rootRef, state, onToggle }: ToolbarProps) => {
         ...css,
       }}
     >
-      <ToggleGroup.Item value="bold">
+      <ToggleGroupItem value="bold">
         <FontBoldIcon />
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="italic">
+      </ToggleGroupItem>
+      <ToggleGroupItem value="italic">
         <FontItalicIcon />
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="link">
+      </ToggleGroupItem>
+      <ToggleGroupItem value="link">
         <Link2Icon />
-      </ToggleGroup.Item>
-    </ToggleGroup.Root>
+      </ToggleGroupItem>
+    </ToggleGroupRoot>
   );
 };
 