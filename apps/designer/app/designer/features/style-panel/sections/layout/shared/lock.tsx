50179f4620509d1068197c69ccb4c2535020176e
Sultan <105234327+natlusyht@users.noreply.github.com>
2022-12-07 20:51:04 +0300

    fixes #538

--- apps/designer/app/designer/features/style-panel/sections/layout/shared/lock.tsx
+++ apps/designer/app/designer/features/style-panel/sections/layout/shared/lock.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Flex, IconButton, Tooltip } from "@webstudio-is/design-system";
 import { LockOpenIcon, LockCloseIcon } from "@webstudio-is/icons";
 import type { Style } from "@webstudio-is/css-data";
@@ -21,15 +21,25 @@ export const Lock = ({
     a?.value + (a?.type === "unit" && a.unit !== "number" ? a.unit : "");
   const bVal =
     b?.value + (b?.type === "unit" && b.unit !== "number" ? b.unit : "");
+  const cVal = useRef(aVal);
   const [isPaired, setIsPaired] = useState(aVal === bVal);
 
+  // @note publish from usePublish seems to have latency issue that causes double updates before resolving to the final update
   useEffect(() => {
     if (isPaired === false) return;
     if (aVal === bVal) return;
-    batchUpdate.setProperty(aKey)(aVal);
-    batchUpdate.setProperty(bKey)(aVal);
-    batchUpdate.publish();
-  }, [aKey, bKey, aVal, bVal, isPaired, batchUpdate]);
+    const currentValue = cVal.current === aVal ? bVal : aVal;
+    cVal.current = currentValue;
+    if (currentValue === bVal) {
+      // bVal changed
+      batchUpdate.setProperty(aKey)(currentValue);
+      batchUpdate.publish();
+    } else {
+      // aVal changed
+      batchUpdate.setProperty(bKey)(currentValue);
+      batchUpdate.publish();
+    }
+  }, [aKey, bKey, aVal, bVal, cVal, isPaired, batchUpdate]);
 
   return (
     <Tooltip
